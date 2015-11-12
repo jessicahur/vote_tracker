d3303631dbfe2e3bf87d02224d30f9745dbca5df
andalex <and4alex@gmail.com>
2015-11-11 16:12:18 -0800

    open weather api integrated, will refactor and refine tomorrow time to git some sleep.

--- js/v.js
+++ js/v.js
@@ -1,6 +1,6 @@
 var img_fn = ["blueMarinatedTurkey.jpg", "condimentSprays.jpg", "cornDogs.jpg",
- "cranberryCandles.jpg", "tteokbokki2.jpg", "frozenWhatAreThese.jpg", "heatedSoda.jpg",
- "jelloMayoTurkey.jpg", "pastaOnionsCandy.jpg", "whipcreamSoup.jpg",
+"cranberryCandles.jpg", "tteokbokki2.jpg", "frozenWhatAreThese.jpg", "heatedSoda.jpg",
+"jelloMayoTurkey.jpg", "pastaOnionsCandy.jpg", "whipcreamSoup.jpg",
 "BotChien.jpg", "BunBoHue.jpg", "OrangeChick.jpg", "StirfriedVeggie.jpg", "VNpancake.jpg",
 "bellPeper.jpg", "bunRieu.jpg", "chickenSalad.jpg", "kimbap.jpg", "frozenShrimp.jpg"];
 
@@ -18,27 +18,24 @@ var btnVote   = $('#btnVote')[0];
 var btnNew    = $('#btnNew')[0];
 var divStats  = $("#divStats")[0];
 var myChart   = $("#myChart")[0];
+var myWeather = $("#myWeather")[0];
 var usr_submit = $('#usr_submit')[0];
 var register = $('#register')[0];
 var you_suck = $('#you_suck')[0];
+var barData = {};
 
 var ctx = myChart.getContext("2d");
+var ctx1 = myWeather.getContext("2d");
 var myChartObj = 0;
 var score_thresh = 50;
 
-register.addEventListener( "click", userReg)
+register.addEventListener( "click", userReg);
 imgLeftE.addEventListener( "click", selectImg);
 imgRightE.addEventListener("click", selectImg);
 btnVote.addEventListener("click", recordVote);
 btnNew.addEventListener("click", newPair);
 
-function userReg () {
-user.their_name =  $('#usr_name').val();
-user.their_city = $('#usr_city').val();
-console.log(user.their_name);
-console.log(user.their_city);
-register.style.visibility="hidden";
-}
+
 
 
 function resetImgs() {
@@ -63,7 +60,7 @@ function img_obj (img_name, vote, score) {
 }
 
 function global_init() {
-var score_array = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 14, -50];
+  var score_array = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 14, -50];
   for (var jj=0; jj < img_fn.length; jj++) {
     var vote = getRandIntOnRange(2, 50);
     working_array.push(new img_obj(img_fn[jj], vote, score_array[jj]));
@@ -83,7 +80,7 @@ function showRandImg() {
   index_1 = getRandIntOnRange(0, (fnPool.length)-1);
   index_2 = getRandIntOnRange(0, (fnPool.length)-1);
 
-while (index_1 === index_2) {
+  while (index_1 === index_2) {
     index_2 = getRandIntOnRange(0, (fnPool.length)-1);
   }
   imgLeftE.src = imgDir + fnPool[index_1].img_name;
@@ -102,46 +99,46 @@ function selectImg() {
 }
 
 function recordVote() {
-    user.count++;
- if (idxSelect == index_1) {
-   console.log("voted. idxSelect="+idxSelect);
-   fnPool[index_1].vote++;
-   voteAllowed = false;
-   btnVote.style.visibility = "hidden";
-   showChart();
-   user.score+=fnPool[index_1].score;
- } else {
-   console.log("voted. idxSelect="+idxSelect);
-   fnPool[index_2].vote++;
-   voteAllowed = false;
-   btnVote.style.visibility = "hidden";
-   showChart();
-   user.score+=fnPool[index_2].score;
- }
- if (index_1 < index_2) {
-   var temp1, temp2;
-   temp1 = fnPool.splice(index_2, 1);
-   intermediate.push(temp1[0]);
-   temp2 = fnPool.splice(index_1, 1);
-   intermediate.push(temp2[0]);
- } else {
-   temp1 = fnPool.splice(index_1, 1);
-   intermediate.push(temp1[0]);
-   temp2 = fnPool.splice(index_2, 1);
-   intermediate.push(temp2[0]);
- }
- console.log(fnPool)
- /*winning or consolation prize section*/
- if (user.count === 15 && user.score < score_thresh) {
-   you_suck.style.visibility = "visible";
-   voteAllowed = false;
+  user.count++;
+  if (idxSelect == index_1) {
+    console.log("voted. idxSelect="+idxSelect);
+    fnPool[index_1].vote++;
+    voteAllowed = false;
+    btnVote.style.visibility = "hidden";
+    showChart();
+    user.score+=fnPool[index_1].score;
+  } else {
+    console.log("voted. idxSelect="+idxSelect);
+    fnPool[index_2].vote++;
+    voteAllowed = false;
+    btnVote.style.visibility = "hidden";
+    showChart();
+    user.score+=fnPool[index_2].score;
+  }
+  if (index_1 < index_2) {
+    var temp1, temp2;
+    temp1 = fnPool.splice(index_2, 1);
+    intermediate.push(temp1[0]);
+    temp2 = fnPool.splice(index_1, 1);
+    intermediate.push(temp2[0]);
+  } else {
+    temp1 = fnPool.splice(index_1, 1);
+    intermediate.push(temp1[0]);
+    temp2 = fnPool.splice(index_2, 1);
+    intermediate.push(temp2[0]);
+  }
+  console.log(fnPool)
+  /*winning or consolation prize section*/
+  if (user.count === 15 && user.score < score_thresh) {
+    you_suck.style.visibility = "visible";
+    voteAllowed = false;
 
- } else if (user.score >= score_thresh) {
-   usr_submit.style.visibility = "visible";
-   vac_input.style.visibility = "visible";
- }
- console.log(user.score);
- console.log(user.count);
+  } else if (user.score >= score_thresh) {
+    usr_submit.style.visibility = "visible";
+    vac_input.style.visibility = "visible";
+  }
+  console.log(user.score);
+  console.log(user.count);
 }
 
 function newPair() {
@@ -196,3 +193,77 @@ btnNew.style.visibility = "visible";
 global_init();
 resetPool();
 showRandImg();
+
+
+/*AJAX*/
+var weatherData = {};
+var temp_min = [];
+var temp_now = [];
+var temp_max = [];
+var k2cOffset = 273.15; // Kelvin vs. Celcius shift
+
+function k2f(x) {
+  return (x - k2cOffset) * 1.8 + 32;
+}
+
+function userReg () {
+  user.their_name =  $('#usr_name').val();
+  user.their_city = $('#usr_city').val();
+  console.log(user.their_name);
+  console.log(user.their_city);
+  register.style.visibility="hidden";
+
+  barData = {
+    labels : ["0","1"],
+    /*labels : [user.their_city+"Min Temp",user.their_city+"Max Temp"],*/
+    datasets : [
+      { fillColor : "rgba(220,220,220,0.5)",
+      strokeColor : "rgba(220,220,220,0.8)",
+      highlightFill: "rgba(220,220,220,0.75)",
+      highlightStroke: "rgba(220,220,220,1)",
+      data : [temp_min[0]]
+    },
+    { fillColor : "rgba(151,187,205,0.5)",
+      strokeColor : "rgba(151,187,205,0.8)",
+      highlightFill : "rgba(151,187,205,0.75)",
+      highlightStroke : "rgba(151,187,205,1)",
+      data : [temp_max[0]]
+//    data : temp_min.slice(0,6)
+//    data : [randGen(),randGen(),randGen(),randGen(),randGen(),randGen(),randGen()]
+    }
+  ]
+  }
+
+$.ajax(
+  { url: "http://api.openweathermap.org/data/2.5/forecast?q="+user.their_city+",us&mode=json&appid=997f0723cfa3d466db2b1cdf7bfa37d6",
+  beforeSend: function(xhr) {
+    //xhr.overrideMimeType("text/plain; charset=x-user-defined");
+  }
+})
+// End of ajax(), but here we do some chaining:
+.done( function(respObj) { // Success
+  console.log("respObj = ", respObj);
+  processResp(respObj);
+  barData.datasets[0].data = temp_min.slice(0,1);
+  barData.datasets[1].data = temp_max.slice(0,1);
+  window.myBar = new Chart(ctx1).Bar(barData, {responsive:true});
+  console.log("Done");
+})
+.fail ( function() {
+  console.log("XHR failed.");
+});
+}
+
+function processResp(rObj) {
+  list = rObj.list;
+
+  for (ii=0; ii < list.length; ii++) {
+    main = list[ii].main;
+    temp_min[ii] = k2f(main.temp_min);
+    temp_max[ii] = k2f(main.temp_max);
+    temp_now[ii] = Math.round(k2f(main.temp_now));
+  }
+
+  console.log("temp_min=" + temp_min);
+  console.log("temp_max=" + temp_max);
+}
